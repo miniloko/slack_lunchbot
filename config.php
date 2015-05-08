c462695bfa4cf7342c70b7e2b3ebcf6bbbb3e5b0
Joakim Lindén <joakimlinden@Macintosh.silverspin.net>
2015-05-08 10:24:20 +0200

    Removed config

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('SLACK_ENDPOINT', ''); // Your endpoint url for Slack: https://hooks.slack.com/services/xxxxxx/xxxxxx/xxxxxxxxxx
-define('SLACK_CHANNEL', '#food');
-define('SLACK_BOT_NAME', 'Lunchbot');
