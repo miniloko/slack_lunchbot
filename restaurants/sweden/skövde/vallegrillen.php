6487e06a8ee69701a596695464bb7664bf6b3ce5
Patrik Freij <patrikfreij@Patriks.silverspin.net>
2015-05-12 12:09:45 +0200

    Adding vallegrillen

---
+++ restaurants/sweden/skövde/vallegrillen.php
@@ -0,0 +1,27 @@
+<?php
+
+function sweden_skövde_vallegrillen()
+{
+	// Vallegrillen
+	// ---------------------
+
+	$client           = getClient();
+	$crawler          = $client->request('GET', 'http://vallegrillen.se/vallegrillen-menu/menu.php');
+	$mealDescriptions = $crawler->filter('article');
+	$output           = array();
+
+	if(count($mealDescriptions)) {
+		$output['restaurant_name'] = 'Vallegrillen';
+		$ix = 0;
+		$dayofweek = date('N'); // ISO-8601 numeric representation of the day of the week
+		$descriptions = $mealDescriptions->eq($dayofweek - 1)->filter('li');
+
+		foreach ($descriptions as $description) {
+			$output['meals'][] = array(
+				'description' => $description->textContent
+			);
+		}
+	}
+
+	return $output;
+}
