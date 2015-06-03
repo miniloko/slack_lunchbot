9d92a1d08716d19c712138290b79a2c9a41156cf
Joakim Lindén <mitt.mailkonto@gmail.com>
2015-06-03 16:02:31 +0200

    Bail if nothing was found.

--- restaurants/sweden/skövde/sand.php
+++ restaurants/sweden/skövde/sand.php
@@ -7,12 +7,16 @@ function sweden_skövde_sand()
 	$foodbox    = $crawler->filter('.menyblack .one-half')->last();
 	$lunchDays  = $foodbox->filter('ul');
 	$dayofweek  = date('N'); // ISO-8601 numeric representation of the day of the week
-	$lunchDay   = $lunchDays->eq($dayofweek - 1)->children();
-	$output['restaurant_name'] = "Restaurang Sand Meza";
-	foreach ($lunchDay as $meal) {
-		$output['meals'][] = array(
-			'description' => $meal->textContent
-		);
+
+	if(count($lunchDays)) {
+		$output['restaurant_name'] = "Restaurang Sand Meza";
+		$lunchDay = $lunchDays->eq($dayofweek - 1)->children();
+
+		foreach ($lunchDay as $meal) {
+			$output['meals'][] = array(
+				'description' => $meal->textContent
+			);
+		}
 	}
 
 	return $output;
