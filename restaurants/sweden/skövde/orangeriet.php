19fbf7cc26e8aa85186637c70a5c6fa5de6dc346
Joakim Lindén <mitt.mailkonto@gmail.com>
2015-10-26 14:27:17 +0100

    Support for weekly specials

--- restaurants/sweden/skövde/orangeriet.php
+++ restaurants/sweden/skövde/orangeriet.php
@@ -1,4 +1,5 @@
 <?php
+use Symfony\Component\DomCrawler\Crawler;
 
 function sweden_skövde_orangeriet()
 {
@@ -9,12 +10,42 @@ function sweden_skövde_orangeriet()
 	$crawler          = $client->request('GET', 'http://mattiasmat.se/');
 	$mealDescriptions = $crawler->filter('#lunch-content-orangeriet h4');
 	$output           = array();
+	$weekdayMap       = array(
+		0 => 'Måndag',
+		1 => 'Tisdag',
+		2 => 'Onsdag',
+		3 => 'Torsdag',
+		4 => 'Fredag',
+	);
+	$specialsMap = array(
+		'Veckans'
+	);
 
-	if(count($mealDescriptions)) {
+	if (count($mealDescriptions)) {
 		$output['name'] = 'Orangeriet';
-		$ix = 0;
 		$dayofweek = date('N'); // ISO-8601 numeric representation of the day of the week
-		$descriptions = explode('<br>', $mealDescriptions->eq($dayofweek - 1)->nextAll()->first()->html());
+		$descriptions = array();
+		$mealDescriptions->each(function (Crawler $node, $i) use ($weekdayMap, $specialsMap, $dayofweek, &$descriptions) {
+			$map = $specialsMap;
+
+			if (!in_array($weekdayMap[$dayofweek - 1], $map)) {
+				$map[] =  $weekdayMap[$dayofweek - 1];
+			}
+
+			foreach ($map as $value) {
+				// Get current weekday lunch
+				if (strpos($node->text(), $value) !== false) {
+					if (in_array($value, $specialsMap)) {
+						$description = $node->text() . ' ' .$node->nextAll()->first()->html();
+					} else {
+						$description = $node->nextAll()->first()->html();
+					}
+
+					$descriptions = array_merge($descriptions, explode('<br>', $description));
+				}
+			}
+
+		});
 
 		foreach ($descriptions as $description) {
 			$output['meals'][] = array(
